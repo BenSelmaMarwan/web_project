0d0358cd7214cc4dadfe6c82c81a993eefe66123
Nicolas Boiteau <nicolas.boiteau@efrei.net>
2023-10-03 23:31:05 +0200

    images

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("heyheyhey")
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Définissez le chemin vers le dossier public pour les fichiers statiques
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route pour la page d'accueil
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','HTML', 'home.html'));
+});
+
+// Port sur lequel l'application Express écoutera
+const port = process.env.PORT || 3000;
+
+// Démarrer le serveur Express
+app.listen(port, () => {
+  console.log(`Serveur Express en cours d'exécution sur le port ${port}`);
+});
